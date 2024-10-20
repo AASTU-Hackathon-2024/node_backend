d6da18d24c59cd273cba952789ae36e4558694ce
YonasWorkneh <yonasdegefu158@gmail.com>
2024-10-20 05:19:00 +0300

    server running

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const env = require("dotenv");
+env.config();
 
-const fetchUsers = async function () {
-  try {
-    const res = await prisma.users.findMany();
-    console.log(res);
-  } catch (err) {
-    console.error(err.message);
-  }
-};
-const createUser = async function (user) {
-  try {
-    const res = await prisma.users.create({
-      data: user,
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
-};
-// model Users {
-//   id        Int      @id @default(autoincrement())
-//   userId    String    @unique @map("user_id")
-//   name      String   @db.VarChar(40)
-//   email     String   @unique @db.VarChar(255)
-//   password  String 
-// }
-const user = {
-    userId:'12@198hk',
-    name:"Yonas Workneh",
-    email:"yonasdegefu158@gmail.com",
-    password:"Inc@rrect_1995"
-}
+const express = require('express');
+const app = express();
 
-createUser(user);
-fetchUsers();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the AASTU-Hackathon Ecommerce platform!");
+  console.log(req);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
