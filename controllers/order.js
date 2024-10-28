a48d506f3f5e7c9e77a04efc7957ef10413ada80
YonasWorkneh <yonasdegefu158@gmail.com>
2024-10-29 01:07:20 +0300

    typo

--- controllers/order.js
+++ controllers/order.js
@@ -37,7 +37,7 @@ const placeOrder = async (req, res) => {
             variationId,
           },
         },
-        product: {
+        proudct: {
           connect: { productId },
         },
         user: {
