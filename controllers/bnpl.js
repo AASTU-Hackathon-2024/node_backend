eb648bde9bd3fccf34ac17ecd2d6c9c3e9a1adf3
YonasWorkneh <yonasdegefu158@gmail.com>
2024-10-30 00:44:44 +0300

    last commit

--- controllers/bnpl.js
+++ controllers/bnpl.js
@@ -1,22 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const uploadKyc = async (req, res) => {
-  //   model Kyc {
-  //   id      Int     @id @default(autoincrement())
-  //   userId  String  @unique @map("user_id")
-  //   idNo    String  @map("id_no")
-  //   idUrl   String  @map("id_url")
-  //   address String
-  //   zipcode String?
-  //   phone   String
-  //   email   String
-  //   status  KycStatus  @default(PENDING)
 
-  //   user User @relation(fields: [userId], references: [userId])
-  // }
-  // const {}
+const uploadKyc = async (req, res) => {
   console.log(req.nody);
+};
+
+const getBnpl = async (req, res) => {
+  console.log(req.body);
+  try {
+    const bnpls = await prisma.bnpl.findMany();
+    res.status(200).json(bnpls);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .json({ message: "Bnpl could not be fetched.", error: err.message });
+  }
 };
 
 const checkLegiblity = async (req, res) => {
@@ -32,11 +32,11 @@ const updateKyc = async (req, res) => {
         status,
       },
     });
-    res.status(200).json({})
+    res.status(200).json({});
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ message: "Error updating kyc", error: err });
   }
 };
 
-export { uploadKyc, checkLegiblity, updateKyc };
+export { uploadKyc, checkLegiblity, updateKyc, getBnpl };
--- routes/bnpl.js
+++ routes/bnpl.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { checkLegiblity, updateKyc, uploadKyc } from "../controllers/bnpl.js";
+import {
+  checkLegiblity,
+  getBnpl,
+  updateKyc,
+  uploadKyc,
+} from "../controllers/bnpl.js";
 
 const bnplRouter = express.Router();
-
 bnplRouter.post("/uploadKyc", uploadKyc);
 bnplRouter.get("/:id", checkLegiblity);
 bnplRouter.post("/update", updateKyc);
+bnplRouter.get("/list", getBnpl);
 
 export default bnplRouter;
